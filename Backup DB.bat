8fcc82efb6dc57ee8da7adb1e47e3bbcce512990
hakor98 <85560489+hakor98@users.noreply.github.com>
2021-06-08 18:31:08 +0200

    push files

---
+++ Backup DB.bat
@@ -0,0 +1,10 @@
+@echo off
+For /f "tokens=2-4 delims=/ " %%a in ('date /t') do (set mydate=%%c-%%a-%%b)
+For /f "tokens=1-2 delims=/:" %%a in ("%TIME%") do (set mytime=%%a%%b)
+ 
+SET backupdir=C:\Users\Administrator\Desktop\BackupDB
+SET mysqluername=root
+SET mysqlpassword=somepassword
+SET database=essentialmode
+ 
+C:\xampp\mysql\bin\mysqldump.exe -uroot  %database% > %backupdir%\%database%_%mydate%_%mytime%_.sql
---
+++ start_3071_default.bat
@@ -0,0 +1,3 @@
+@echo off
+C:/Users/Administrator/Desktop/Inside_4.0/data//FXServer.exe +set serverProfile "default"
+pause
